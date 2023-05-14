c9d618a2214a085d646c85438434158cca88923b
TannyKorry <tannykorry@gmail.com>
2023-05-14 22:13:10 +0300

    ProgrammingError at /admin/articles/rubric/

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Rubric, RubricArticles
 
+
+class RubricArticlesInline(admin.TabularInline):
+    model = RubricArticles
+    extra = 1
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'published_at']
+    list_filter = ['title']
+
+
+@admin.register(Rubric)
+class RubricAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    inlines = [RubricArticlesInline, ]
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+class Rubric(models.Model):
+
+    name = models.CharField(max_length=50, verbose_name='Рубрика')
+
+    class Meta:
+        verbose_name = 'Рубрика'
+        verbose_name_plural = 'Рубрики'
+
+    def __str__(self):
+        return self.name
+
+
 
 class Article(models.Model):
 
@@ -7,10 +19,21 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    topic = models.ManyToManyField('Rubric', related_name='articles', through='RubricArticles')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class RubricArticles(models.Model):
+    tag = models.ForeignKey('Rubric', on_delete=models.CASCADE, related_name='art')
+    article = models.ForeignKey('Article', on_delete=models.CASCADE, related_name='art')
+    is_main = models.BooleanField(verbose_name='Основной')
+
+    def __str__(self):
+        return f'{self.tag} {self.article}'
